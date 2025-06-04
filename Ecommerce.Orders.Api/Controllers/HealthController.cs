df0d0bfafd57c9d1dc1755511187946af3f636e0
matusalik <mateuszsalik@gmail.com>
2025-06-04 23:03:03 +0200

    Added HealthController for troublehooting

---
+++ Ecommerce.Orders.Api/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Orders.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet("ping")]
+    public IActionResult Ping()
+    {
+        return Ok("API is alive");
+    }
+}
+
